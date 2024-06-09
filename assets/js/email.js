b09890d1997e1ed449b9d42fe068b3a7af82651d
kalebeccs <kalebe.correa.santos@gmail.com>
2024-06-09 16:15:37 +0100

    Update email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,10 +1,9 @@
 const contactForm = document.getElementById('contact-form'),
-    contactMessage = document.getElementById('contact-status')
+      contactMessage = document.getElementById('contact-status')
 
 const sendEmail = (e) => {
     e.preventDefault()
 
-    //serviceID - templateID - #form - publicKey
     emailjs.sendForm('service_3qpu9j9', 'template_fztnj8a', '#contact-form', 'DZFmbza9T7-mhF6wS')
         .then(() => {
             contactMessage.textContent = 'Message sent successfully ✅'
