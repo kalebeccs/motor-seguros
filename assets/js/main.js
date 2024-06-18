0dd1f9d7506dd0fc3bca97d1f3fca94080b350e1
kalebeccs <kalebe.correa.santos@gmail.com>
2024-06-18 21:58:03 +0100

    Update index.html
    Change the comments and advertisement part

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
+/*=============== SWIPER PROMOTION AND BENEFITS ===============*/
 let swiperCards = new Swiper('.card__content', {
+    loop: true,
+    spaceBetween: 0,
+    grabCursor: true,
+// Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+/*=============== SWIPER INDEX ===============*/
+let swiperCards2 = new Swiper('.comment__content', {
     loop: true,
     spaceBetween: 0,
     grabCursor: true,
