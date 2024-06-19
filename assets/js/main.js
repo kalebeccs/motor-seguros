861eff76edeb66408fc4bd92e56275550db8c794
kalebeccs <kalebe.correa.santos@gmail.com>
2024-06-19 12:36:48 +0100

    Update carousel section in pb page
    small adjustments to the html, css and js files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
 /*=============== SWIPER PROMOTION AND BENEFITS ===============*/
-let swiperCards = new Swiper('.card__content', {
+let swiperCards = new Swiper('.carousel__content', {
     loop: true,
     spaceBetween: 0,
     grabCursor: true,
-// Navigation arrows
+
+    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
 
-    autoplay: {   //native from Swiper library
+    //autoplay
+    autoplay: {
         delay: 3000,
         disableOnInteraction: false, 
     }
@@ -20,7 +22,8 @@ let swiperCards2 = new Swiper('.comment__content', {
     loop: true,
     spaceBetween: 0,
     grabCursor: true,
-// Navigation arrows
+
+    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
