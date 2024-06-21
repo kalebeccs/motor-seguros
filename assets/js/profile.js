95734d5531fe882d6bb3a7df3872945108bc79b4
kalebeccs <kalebe.correa.santos@gmail.com>
2024-06-21 13:42:21 +0100

    add address in profile

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -2,6 +2,7 @@
 const profileName = document.getElementById('profile-name'),
     profileAvatar = document.getElementById('profile-avatar'),
     profileEmail = document.getElementById('profile-email'),
+    profileAddress = document.getElementById('profile-address'),
     exitBtn = document.getElementById('nav-exit')
 
 // Get the user from the local storage
@@ -14,7 +15,8 @@ if(user == null) {
 // Set the values to the elements
 profileAvatar.src = `https://github.com/${user.username}.png`
 profileName.textContent = `${user.firstname} ${user.lastname}`
-profileEmail.textContent = user.email;
+profileEmail.textContent = user.email
+profileAddress.textContent = user.address
 
 exitBtn.addEventListener('click', () => {
     sessionStorage.clear();
