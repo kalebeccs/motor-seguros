ee19db12eb3f8774e1d21181d3e9319fabb57a09
kalebeccs <kalebe.correa.santos@gmail.com>
2024-06-21 13:52:46 +0100

    add phone in profile

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -2,6 +2,7 @@
 const profileName = document.getElementById('profile-name'),
     profileAvatar = document.getElementById('profile-avatar'),
     profileEmail = document.getElementById('profile-email'),
+    profilePhone = document.getElementById('profile-phone'),
     profileAddress = document.getElementById('profile-address'),
     exitBtn = document.getElementById('nav-exit')
 
@@ -16,6 +17,7 @@ if(user == null) {
 profileAvatar.src = `https://github.com/${user.username}.png`
 profileName.textContent = `${user.firstname} ${user.lastname}`
 profileEmail.textContent = user.email
+profilePhone.textContent = user.phone
 profileAddress.textContent = user.address
 
 exitBtn.addEventListener('click', () => {
