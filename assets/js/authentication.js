68e4fd5de6af3ad726592fc514a54e0268cf788c
kalebeccs <kalebe.correa.santos@gmail.com>
2024-07-12 13:43:35 +0100

    Update authentication.js
    removing user creation

--- assets/js/authentication.js
+++ assets/js/authentication.js
@@ -1,7 +1,3 @@
-// -------------------- AUTHENTICATION -------------------- //
-// This script handles the login and sign up functionality //
-// ------------------------------------------------------ //
-
 // -----------------  LOGIN ----------------- //
 // References to the necessary DOM elements: 
 const email = document.getElementById('email'),
@@ -49,78 +45,4 @@ async function getData(src) {
     const response = await fetch(src);
     const responseJson = await response.json();
     return responseJson;
-}
-
-// -----------------  SIGN UP ----------------- //
-
-// References to the necessary DOM elements:
-const fName = document.getElementById('fName'),
-    lName = document.getElementById('lName'),
-    signUpEmail = document.getElementById('email'),
-    signUpPassword = document.getElementById('password'),
-    signUpSubmit = document.getElementById('signUp__button');
-
-// An event listener is added to the Sign Up button to trigger the sign up process when clicked.
-signSubmit.addEventListener('click', (e) => {
-    e.preventDefault();
-    trySignUp(signUpEmail.value, signUpPassword.value, fName.value, lName.value);
-})
-
-// Authentication for sign up
-function trySignUp() {
-    if (!("@gmail.com" in email)) {
-        statusSpan.textContent = "Wrong email format.";
-    }
-    if (password.length < 8) {
-        statusSpan.textContent = "Password must be at least 8 characters.";
-    }
-    if (password.length > 16) {
-        statusSpan.textContent = "Password must be at most 16 characters.";
-    }
-    if (password.value.match(/[A-Z]/) == null) {
-        statusSpan.textContent = "Password must contain at least one uppercase letter.";
-    }
-    if (password.value.match(/[a-z]/) == null) {
-        statusSpan.textContent = "Password must contain at least one lowercase letter.";
-    }
-    if (password.value.match(/[0-9]/) == null) {
-        statusSpan.textContent = "Password must contain at least one number.";
-    }
-    if (password.value.match(/[!@#$%^&*]/) == null) {
-        statusSpan.textContent = "Password must contain at least one special character.";
-    }
-    else {
-        register_user();
-    }
-}
-
-// Registers a new user
-function register_user (fName, lName, email, password) {
-
-    // User data
-    const userData = {
-        fName: fName.value,
-        lName: lName.value,
-        email: email.value,
-        password: password.value
-    };
-
-    // Fetches the data from the JSON file
-    fetch('./assets/json/data.json', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        // Converts the data to a JSON string
-        body: JSON.stringify(userData)
-    })
-        // Converts the response to a JSON object and try to log the data
-        .then(response => response.json())
-        .then(data => {
-            statusSpan.textContent = "Sign up successful!";
-        })
-        .catch(error => {
-            statusSpan.textContent = "Error occurred while signing up.";
-            console.error(error);
-        });
 }
